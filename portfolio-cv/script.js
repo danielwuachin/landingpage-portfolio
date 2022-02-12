0b459a3ca2ce965a0f6cf5c3c4ddf91977b9c6ba
danielwuachin <j.d.rodriguez.1624@gmail.com>
2022-02-12 12:38:02 -0400

    changed the email in form to j.d.rodri...

--- portfolio-cv/script.js
+++ portfolio-cv/script.js
@@ -34,7 +34,7 @@
     $form.addEventListener("submit", (e) => {
       e.preventDefault();
       $loader.classList.remove("none");
-      fetch("https://formsubmit.co/ajax/danielwuachin@gmail.com", {
+      fetch("https://formsubmit.co/ajax/j.d.rodriguez.1624@gmail.com", {
         method: "POST",
         body: new FormData(e.target),
       })
